eeaded9105e1b5292a96104f26a3883859975606
Harish-senthil <162134443+Harish-senthil@users.noreply.github.com>
2024-03-27 20:04:26 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -22,18 +22,34 @@ function submit_create(tagname, content) {
     var submit = document.createElement(tagname);
     submit.setAttribute("type", "submit");
     submit.innerHTML = content;
+
+    submit.addEventListener("click", function() {
+        var firstNameValue = document.getElementById("Firstname").value;
+        var middleNameValue = document.getElementById("Middlename").value;
+        var lastNameValue = document.getElementById("Lastname").value;
+        var emailValue = document.getElementById("Email").value;
+        var addressValue = document.getElementById("Address").value;
+
+        console.log(firstNameValue);
+        console.log(middleNameValue);
+        console.log(lastNameValue);
+        console.log(emailValue);
+        console.log(addressValue);
+    });
+
     return submit;
 }
 
-var firstName = label_create("label","for","fname","First Name");
+
+var firstName = label_create("label","for","firstname","First Name");
 let firstbreak = break_create()
 let firstinp = input_create("input","type","text","Firstname")
 let firstinp_br = break_create()
-var middleName = label_create("label","for","mname","Middle Name");
+var middleName = label_create("label","for","middlename","Middle Name");
 let middlebreak = break_create()
 let middleinp = input_create("input","type","text","Middlename")
 let middleinp_br = break_create()
-var lastName = label_create("label","for","lname","Last Name");
+var lastName = label_create("label","for","lastname","Last Name");
 let lastbreak = break_create()
 let lastinp = input_create("input","type","text","Lastname")
 let lastinp_br = break_create()
@@ -41,7 +57,7 @@ var email = label_create("label","for","email","Email");
 let emailbreak = break_create()
 let emailinp = input_create("input","type","email","Email")
 let emailinp_br = break_create()
-var address = label_create("label","for","addr","Address");
+var address = label_create("label","for","address","Address");
 let addbreak = break_create()
 let addinp = input_create("input","type","textarea","Address")
 let addinp_br = break_create()
